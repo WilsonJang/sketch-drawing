91361ab80783fdf7638a52ac923354e61bf18e30
Wilson <Wilsonjang10@gmail.com>
2021-02-24 16:36:00 -0500

    minor fixes, changed UI

--- index.js
+++ index.js
@@ -14,24 +14,27 @@ slider.addEventListener("mouseup", () => {
   createGrid(slider.value);
 });
 
+
 //hold to color
 container.addEventListener("mousedown", () => {
+  onclick= true;
   mousedown = true;
 });
 //release to not color
 container.addEventListener("mouseup", () => {
+  onclick=false;
   mousedown = false;
 });
 let eraseOn = false;
 //color method
 function onColor(e) {
-  if (!mousedown) {
+  if (!mousedown && !onclick) {
     return;
   }
   if (!eraseOn) {
     e.target.style.backgroundColor = colorWell.value;
   } else {
-    e.target.style.backgroundColor = "#ffffff";
+    e.target.style.backgroundColor = "transparent";
   }
 }
 
@@ -54,6 +57,7 @@ function createGrid(square) {
       box.classList.add("boxStyles");
       box.style.width = 600 / square + "px";
       box.style.height = 600 / square + "px";
+      box.onclick = onColor;
       box.onmouseover = onColor;
       container.append(box);
     }
